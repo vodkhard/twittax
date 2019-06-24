607b7b65c012a3e36eb5318b96c9eda22d03b86b
Hugo Knorr <hugo.knorr@gmail.com>
2019-06-24 10:53:03 +0200

    :sparkles: add app-icon component

--- js/components/twaat/item.js
+++ js/components/twaat/item.js
@@ -1,9 +1,10 @@
 import { LitElement, html, css } from 'lit-element';
 import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime'
+import relativeTime from 'dayjs/plugin/relativeTime';
 import twaatsRepository from '../../data/repositroy/twaats';
 import './comment';
 import './content';
+import '../ui/icon';
 
 dayjs.extend(relativeTime);
 
@@ -121,27 +122,6 @@ class Item extends LitElement {
         margin-left: -5px;
         margin-right: -5px;
         display: flex;
-      }
-      .icone {
-        max-width: 49px;
-        box-align: end;
-        align-items: flex-end;
-        min-width: 0px;
-        flex-basis: 0px;
-        box-flex: 1;
-        flex-grow: 1;
-        margin-left: 5px;
-        margin-right: 3px;
-      }
-      svg {
-        width: 12px;
-        color: #657786;
-        fill: #657786;
-        vertical-align: text-bottom;
-        position: relative;
-        height: 1.35em;
-        display: inline-block;
-        float: right;
       }
       .text {
         box-flex: 7;
@@ -294,37 +274,22 @@ class Item extends LitElement {
           ${this.child
             && html`
               <div class="retweet">
-                <div class="icone">
-                  <svg viewBox="0 0 24 24">
-                    <g>
-                      <path
-                        d="M23.615 15.477c-.47-.47-1.23-.47-1.697 0l-1.326 1.326V7.4c0-2.178-1.772-3.95-3.95-3.95h-5.2c-.663 0-1.2.538-1.2 1.2s.537 1.2 1.2 1.2h5.2c.854 0 1.55.695 1.55 1.55v9.403l-1.326-1.326c-.47-.47-1.23-.47-1.697 0s-.47 1.23 0 1.697l3.374 3.375c.234.233.542.35.85.35s.613-.116.848-.35l3.375-3.376c.467-.47.467-1.23-.002-1.697zM12.562 18.5h-5.2c-.854 0-1.55-.695-1.55-1.55V7.547l1.326 1.326c.234.235.542.352.848.352s.614-.117.85-.352c.468-.47.468-1.23 0-1.697L5.46 3.8c-.47-.468-1.23-.468-1.697 0L.388 7.177c-.47.47-.47 1.23 0 1.697s1.23.47 1.697 0L3.41 7.547v9.403c0 2.178 1.773 3.95 3.95 3.95h5.2c.664 0 1.2-.538 1.2-1.2s-.535-1.2-1.198-1.2z"
-                      ></path>
-                    </g>
-                  </svg>
-                </div>
+                <app-icon icon="retwaat"></app-icon>
                 <div class="text">
                   <span>retweeté</span>
                 </div>
               </div>
             `}
-          ${this.item.parent
-            && html`
-              <div class="retweet">
-                <div class="icone">
-                  <svg viewBox="0 0 24 24">
-                    <g>
-                      <path
-                        d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788z"
-                      ></path>
-                    </g>
-                  </svg>
+          
+            ${this.item.parent
+              && html`
+                <div class="retweet">
+                  <app-icon icon="retwaat"></app-icon>
+                  <div class="text">
+                    <span>répondu</span>
+                  </div>
                 </div>
-                <div class="text">
-                  <span>répondu</span>
-                </div>
-              </div>
-            `}
+              `}
 
           <div class="tweet">
             <div class="left">
@@ -368,64 +333,28 @@ class Item extends LitElement {
               <div class="buttons">
                 <div class="button">
                   <div class="button-svg" @click=${this.onComment}>
-                    <svg
-                      viewBox="0 0 24 24"
-                      class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
-                    >
-                      <g>
-                        <path
-                          d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"
-                        ></path>
-                      </g>
-                    </svg>
+                    <app-icon icon="comment"></app-icon>
                   </div>
                   <div class="count"><span>0</span></div>
                 </div>
 
                 <div class="button">
                   <div class="button-svg" @click=${this.onRetwaat}>
-                    <svg
-                      viewBox="0 0 24 24"
-                      class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
-                    >
-                      <g>
-                        <path
-                          d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"
-                        ></path>
-                      </g>
-                    </svg>
+                    <app-icon icon="retwaat"></app-icon>
                   </div>
                   <div class="count"><span>${this.item.retwaats}</span></div>
                 </div>
 
                 <div class="button">
                   <div class="button-svg" @click=${() => this.onUpdate('like')}>
-                    <svg
-                      viewBox="0 0 24 24"
-                      class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1hdv0qi"
-                    >
-                      <g>
-                        <path
-                          d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"
-                        ></path>
-                      </g>
-                    </svg>
+                    <app-icon icon="like"></app-icon>
                   </div>
                   <div class="count"><span>${this.item.like}</span></div>
                 </div>
 
                 <div class="button" title="Supprimer">
                   <div class="button-svg" @click=${this.onDelete}>
-                    <svg
-                      viewBox="0 0 24 24"
-                      class="r-1re7ezh r-4qtqp9 r-yyyyoo r-1q142lx r-1xvli5t r-zso239 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"
-                    >
-                      <g>
-                        <path
-                          d="M12 1.25C6.072 1.25 1.25 6.072 1.25 12S6.072 22.75 12 22.75 22.75 17.928 22.75 12 17.928 1.25 12 1.25zm0 1.5c2.28 0 4.368.834 5.982 2.207L4.957 17.982C3.584 16.368 2.75 14.282 2.75 12c0-5.1 4.15-9.25 9.25-9.25zm0 18.5c-2.28 0-4.368-.834-5.982-2.207L19.043 6.018c1.373 1.614 2.207 3.7 2.207 5.982 0 5.1-4.15 9.25-9.25 9.25z"
-                        ></path>
-                      </g>
-                    </svg>
+                    <app-icon icon="delete"></app-icon>
                   </div>
                 </div>
               </div>
