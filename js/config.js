503f92b4ba4b8a3d631b6fb55105adebdf6e3ae2
Valentin Varveropoulos <valentin.varveropoulos@hotmail.fr>
2019-06-30 17:32:18 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: 'AIzaSyCZCyCIzUnNB-SrTtUbraQmSUufE7zF0WE',
-  authDomain: 'twittax-2f106.firebaseapp.com',
-  databaseURL: 'https://twittax-2f106.firebaseio.com',
-  projectId: 'twittax-2f106',
-  storageBucket: 'gs://twittax-2f106.appspot.com/',
-  messagingSenderId: '584378516458',
-  appId: '1:584378516458:web:1a846e064c4b4771',
+  apiKey: 'AIzaSyBxmZWoNJUB4pXBX1sWG2F56-cHzZoc26E',
+  authDomain: 'twittax-esgi.firebaseapp.com',
+  databaseURL: 'https://twittax-esgi.firebaseio.com',
+  projectId: 'twittax-esgi',
+  storageBucket: '',
+  messagingSenderId: '638172554520',
+  appId: '1:638172554520:web:95cc60abb5776661',
 };
 
 export default config;
