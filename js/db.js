ea62403ad9071c67cca8fec1edcc392e89bb0cc0
Hugo Knorr <hugo.knorr@gmail.com>
2019-07-19 00:12:58 +0200

    :sparkles: Add twaat modal with pic upload

--- js/db.js
+++ js/db.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 import config from './config';
 
 firebase.initializeApp(config);
@@ -9,3 +10,4 @@ firebase.firestore().enablePersistence();
 export const fieldValue = firebase.firestore.FieldValue;
 export const firestore = firebase.firestore();
 export const fireauth = firebase.auth();
+export const firestorage = firebase.storage();
